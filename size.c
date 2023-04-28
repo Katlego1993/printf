54c9020ab1c63b2fa89f1ef2d6c72a7e74f90918
Lloyd Madiba <125538016+SirLloyds@users.noreply.github.com>
2023-04-28 23:22:58 +0200

    Update size.c

--- size.c
+++ size.c
@@ -2,6 +2,7 @@
 
 /**
  * get_size - Calculates the size tocast the arguments
+ * kat & lloyd project
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed
  *
