3e759100b7d6f5e8efa4cebde0f3180961d52541
katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-04-28 05:47:53 -0700

    precision.c

--- precision.c
+++ precision.c
@@ -22,7 +22,7 @@ for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 if (is_digit(format[curr_i]))
 {
 precision *= 10;
-precision += format [curr_i] - '0';
+precision += format[curr_i] - '0';
 }
 else if (format[curr_i] == '*')
 {
